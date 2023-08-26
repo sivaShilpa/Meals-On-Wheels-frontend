748fc08e8ed0b06b7cf181a5ad5c908a8ef36094
sivaShilpa <shilpa.vchand@gmail.com>
2023-08-26 16:17:06 -0400

    modify header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,10 @@
 function Footer(props) {
-    return <h1>Footer goes here</h1>;
+    return (
+      <footer>
+        <h1>Footer goes here</h1>
+      </footer>
+    
+    );
   }
   
   export default Footer;
