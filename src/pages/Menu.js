a59d41bd28cc598053dc6aaef980a7fb1277466a
sivaShilpa <shilpa.vchand@gmail.com>
2023-08-31 09:19:46 -0400

    menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "../App.css"
 
 function Menu(props) {
   // create state to hold projects
@@ -14,7 +15,10 @@ function Menu(props) {
     const data = await response.json();
 
 		// set the projects state to the data
-    setMenu(data);
+    if(response.ok){
+      setMenu(data);
+    }
+    
 
   };
 
@@ -22,20 +26,30 @@ function Menu(props) {
   useEffect(() =>{getMenuData()}, []);
 
   // define a function that will return the JSX needed once we get the data
-  const loaded = () => {
-    return menu.map((dish) => (
-      <div>
-        <h1>{dish.name}</h1>
-        <img src={dish.image} alt=""/>
-        <p>{dish.description}</p>
-        
-        <button>add to cart</button>
-      
-      </div>
-    ));
-  };
+  // const loaded = () => {
+  //   return menu.map((dish) => (      
+  //       <div className="eachMenu">
+  //         <h1>{dish.name}</h1>
+  //         <img src={dish.image} alt=""/>
+  //         <p>{dish.description}</p>
+  //       </div>      
+  //   ));
+  // };
 
-  return menu ? loaded() : <h1>Loading...</h1>;
+  // return (<div className="menuPage"> {menu} ? {loaded()} : <h1>Loading...</h1></div>);
+  return(
+    <div className="menuPage">
+      <div className="eachMenu">
+        {menu && menu.map((dish)=>(
+          <div className="menuDetails">
+            <h1 key={dish._id}>{dish.name}</h1>
+            <img src={dish.image} alt=""/>
+            <p>{dish.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
