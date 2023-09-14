3de600777583b92024dbd6681010452408672dd7
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-14 09:01:40 -0400

    hamburger menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -29,7 +29,24 @@ function Menu(props) {
 
   // define a function that will return the JSX needed once we get the data
    return (
-    <div className="menuPage"> 
+    <div className="menuPage">
+       <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>  
+          <div class="menu-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+          </div>
+        </div>
+      </div>
       <div className="eachMenu">
         {menu && menu.map((meal)=>(
           <div className="menuDetails">
